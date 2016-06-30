b831dedf008ea618bc92713cedc22261142e0e3a
Larry Reid <lcreid@jadesystems.ca>
2016-06-30 15:22:26 -0700

    Address can have no province.
    Rails 5 requires `, optional: true` on a `belongs_to` relationship
    if you might not always have something on the other end of the
    relationship (e.g. lookup tables, membership in a group that's
    optional).

--- test/models/address_test.rb
+++ test/models/address_test.rb
@@ -5,11 +5,10 @@ class AddressTest < ActiveSupport::TestCase
     address = Address.new do |a|
       a.line1 = '2737 E Pender St'
       a.city = 'Vancouver'
-      a.province = provinces(:bc)
       a.postal_code = 'V5K 2B9'
     end
 
     assert address.valid?, 'address not valid'
-    assert address.save, 'address could not be save'
+    assert address.save, 'address could not be saved'
   end
 end
