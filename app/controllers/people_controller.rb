7b29382346366043ec66f8d61e7d742893feb4d4
Larry Reid <lcreid@jadesystems.ca>
2016-09-04 09:07:37 -0700

    CRUD on phone association.
    This is the normal, forward, direction. I wanted to do the reverse
    direction (edit the "many", but also edit its "belongs to"), but
    I spent too much time one: Turbolinks -- always remember to do
    JavaScript ready the Rails way -- and errors caused by leaving
    debugging in my JavaScript, that caused all sorts of havoc.
    Don't get complacent or sloppy when removing debugging after a
    session of flailing.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -3,11 +3,44 @@ class PeopleController < ApplicationController
     @people = Person.all
   end
 
+  def show
+    @person = Person.find(params[:id])
+  end
+
   def new
     @person = Person.new
   end
 
   def edit
     @person = Person.find(params[:id])
+  end
+
+  def create
+    # puts pp(params.as_json)
+    @person = Person.new(person_params)
+    if @person.save(person_params)
+      redirect_to person_path(@person)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @person = Person.find(params[:id])
+    if @person.update(person_params)
+      redirect_to person_path(@person)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name,
+                                   :id,
+                                   phones_attributes: [
+                                     :id, :phone_type, :number, :_destroy
+                                   ])
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+require "capybara/rails"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
@@ -8,3 +9,27 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+# I found that with Rails 5, IntegrationTest is used for controller tests, too,
+# and things get weird if you put the Capybara stuff inside the controller
+# tests. So I make a new class for Capybara tests, instead of patching the
+# integration test class.
+class CapybaraTestCase < ActionDispatch::IntegrationTest
+  include Capybara::DSL
+
+  def teardown
+    Capybara.reset_sessions!
+    Capybara.use_default_driver
+  end
+end
+
+# I got this from: https://github.com/chriskottom/minitest_cookbook_source/issues/3
+# To fix transacation issues with Poltergeist tests
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+  def self.connection
+    @@shared_connection || retrieve_connection
+  end
+end
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
